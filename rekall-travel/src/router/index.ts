b21d2587384c73d59726b0c734a6dbb9b39528df
Natalie1788 <Alextrazzza@gmail.com>
2024-12-11 12:41:41 +0100

    special offers

--- rekall-travel/src/router/index.ts
+++ rekall-travel/src/router/index.ts
@@ -8,12 +8,14 @@ import DestinationsView from '@/views/DestinationsView.vue'
 import Mars from '@/components/AboutMars.vue'
 import Venus from '@/components/AboutVenus.vue'
 import Sun from '@/components/AboutSun.vue'
+import Offers from '@/views/Offers.vue'
 
 const routes = [
   { path: '/', name: 'home', component: HomeView },
   { path: '/blog', name: 'blog', component: BlogView },
   { path: '/blog/:id', name: 'blogPost', component: BlogPostView, props: true },
   { path: '/hotels', name: 'hotels', component: HotelsView },
+  { path: '/offers', name: 'offers', component: Offers },
   {
     path: '/destinations',
     component: DestinationsView,
