4fa6bf230463e669c61b87868c6c9c70542b7ad3
AndoGPP <146361135+AndoGPP@users.noreply.github.com>
2024-12-16 14:25:14 +0100

    added price

--- rekall-travel/src/views/ExcursionsView.vue
+++ rekall-travel/src/views/ExcursionsView.vue
@@ -1,16 +1,13 @@
 <template>
-  <div class="excursions-page" >
+  <div class="excursions-page">
     <h1>Excursions for {{ planetName }}</h1>
+    <TotalPrice />
     <div v-if="filteredExcursions.length > 0">
-      <div
-        v-for="excursion in filteredExcursions"
-        :key="excursion.id"
-        class="excursion-item"
-      >
+      <div v-for="excursion in filteredExcursions" :key="excursion.id" class="excursion-item">
         <h2>{{ excursion.name }}</h2>
         <p>{{ excursion.description }}</p>
         <p><strong>Duration:</strong> {{ excursion.duration }}</p>
-        <p><strong>Price:</strong> {{ excursion.price}}</p>
+        <p><strong>Price:</strong> {{ excursion.price }}</p>
         <button @click="addToCart(excursion)">Add to Cart</button>
       </div>
     </div>
@@ -21,10 +18,11 @@
 </template>
 
 <script setup lang="ts">
-import { useCartStore } from '@/stores/cart';
+import { useCartStore } from '@/stores/cart'
 import { computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import excursionsData from '@/db/excursions.json' // Import JSON data
+import TotalPrice from '@/components/TotalPrice.vue'
 
 const route = useRoute()
 const router = useRouter()
@@ -39,8 +37,8 @@ const addToCart = (excursion: { id: string; name: string; price: number }) => {
       excursionName: excursion.name,
       excursionPrice: excursion.price.toString(),
     },
-  });
-};
+  })
+}
 
 // Extract destination from query parameters
 const destination = computed(() => route.query.destination as string)
@@ -56,11 +54,8 @@ const planetName = computed(() => {
 // Filter excursions where the id contains the planet name
 const excursions = excursionsData
 const filteredExcursions = computed(() =>
-  excursions.filter((exc) =>
-    planetName.value && exc.id.toLowerCase().includes(planetName.value)
-  )
+  excursions.filter((exc) => planetName.value && exc.id.toLowerCase().includes(planetName.value)),
 )
-
 </script>
 
 <style scoped>
